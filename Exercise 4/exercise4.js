c2e665ac6782b35294bc96122d0b415f3b356e53
amiin5377-amo <amiin5377@gmail.com>
2026-06-30 20:35:31 +0200

    submitted exercise4

---
+++ Exercise 4/exercise4.js
@@ -0,0 +1,18 @@
+function add(a , b) {
+    console.log(a +b);
+
+}
+ add(5, 10);
+add(4, 8);
+add(6, 8);
+
+
+const adds = function(a , b) {
+    console.log(a + b);
+
+}
+
+adds(5, 5);
+adds(4, 5);
+adds(6,5);
+
