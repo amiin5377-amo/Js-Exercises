01fc22e3e42aa9fc4eada6cc1553c6c0db76e406
amiin5377-amo <amiin5377@gmail.com>
2026-07-03 17:23:05 +0200

    submitted exercise8

---
+++ Exercise 8/exercise8.js
@@ -0,0 +1,9 @@
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+
+console.log(car.make); // Output: Toyota
+console.log(car.model); // Output: Camry
+console.log(car.year); // Output: 2020
