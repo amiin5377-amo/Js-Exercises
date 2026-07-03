e587f0a6de62ad80458c6ac5729e3b52f3d202eb
amiin5377-amo <amiin5377@gmail.com>
2026-07-03 17:58:30 +0200

    submiitted exercise 9

---
+++ Exercise 9/exercise9.js
@@ -0,0 +1,10 @@
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    start: function() {
+        console.log("The car has started.");
+    }
+};
+
+console.log(car.start()); // Output: The car has started.
