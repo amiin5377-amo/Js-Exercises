ea74d0341a04fba72e53b2fa63d071b9ab0c432f
amiin5377-amo <amiin5377@gmail.com>
2026-07-03 18:48:46 +0200

    submitted exercise10

---
+++ Exercise 10/exercise10.js
@@ -0,0 +1,16 @@
+let library = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    },
+
+
+];
+console.log(library[0].title); // Output: The Great Gatsby
+console.log(library[1].author); // Output: Harper Lee
