cf4c357a6b14a5a65b349c0abe91b9f8ada0ca32
amiin5377-amo <amiin5377@gmail.com>
2026-07-03 16:05:09 +0200

    submitted exercise6

---
+++ Exercise 6/exercise6.js
@@ -0,0 +1,8 @@
+let colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
+console.log(colors[0]); // Output: red
+console.log(colors[1]); // Output: blue
+console.log(colors[2]); // Output: green
+console.log(colors[3]); // Output: yellow
+console.log(colors[4]); // Output: orange
+console.log(colors[5]); // Output: purple
+console.log(colors[6]); // Output: pink
