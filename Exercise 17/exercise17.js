120bf92f6b0c3f891dd29310fedaef5eba8aec5f
amiin5377-amo <amiin5377@gmail.com>
2026-07-06 22:07:12 +0200

    submitted exercise17

---
+++ Exercise 17/exercise17.js
@@ -0,0 +1,13 @@
+let temperaturure = 30;
+
+if (temperaturure < 0) {
+    console.log("It's freezing outside!");
+}
+
+else if (temperaturure >= 0 && temperaturure <= 15) {
+    console.log("It's quite cold outside.");
+} else if (temperaturure > 15 && temperaturure <= 25) {
+    console.log("The weather is pleasant.");
+} else {
+    console.log("It's hot outside!");
+}   
