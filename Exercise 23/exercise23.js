c664fcd0311218c9519d3018be012d823d2fd00b
amiin5377-amo <amiin5377@gmail.com>
2026-07-12 18:19:19 +0200

    done

---
+++ Exercise 23/exercise23.js
@@ -0,0 +1,12 @@
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "blue"
+};
+
+let { make, model, year, color } = car;
+console.log(make);
+console.log(model);
+console.log(year);
+console.log(color);
