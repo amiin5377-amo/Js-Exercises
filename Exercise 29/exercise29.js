4f599920c3df6019fa9ae184b7d9f48deb2a3c13
amiin5377-amo <amiin5377@gmail.com>
2026-07-16 01:20:00 +0200

    all done

---
+++ Exercise 29/exercise29.js
@@ -0,0 +1,7 @@
+async function fetchUserData() {
+const user = await fetch('https://jsonplaceholder.typicode.com/users/1');
+const userData = await user.json();
+console.log("Fetched user data:", userData);
+}
+
+fetchUserData();
