11047008cb8d1111b1717ef118817b1af05c9180
amiin5377-amo <amiin5377@gmail.com>
2026-07-19 17:56:20 +0200

    all done

---
+++ Exercise 32/exercise32.js
@@ -0,0 +1,7 @@
+const portfolioTitle = document.querySelector('#portfolio-title');
+const portfolioDescription = document.querySelectorAll('.portfolio-description');
+const Button = document.querySelector('button');
+
+console.log(portfolioTitle);
+console.log(portfolioDescription);
+console.log(Button);
