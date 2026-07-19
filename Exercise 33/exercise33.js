5683889b54584205fe730d8a3efd38db94cba2fe
amiin5377-amo <amiin5377@gmail.com>
2026-07-19 22:00:37 +0200

    finished

---
+++ Exercise 33/exercise33.js
@@ -0,0 +1,9 @@
+function changeElement() {
+    const h1Element = document.querySelector('h1');
+    h1Element.innerHTML ="<h6>Welcome to JavaScript!</h6>" 
+}
+
+function changeContent() {
+    const paragraphElement = document.querySelector('p');
+    paragraphElement.textContent = "This is a new paragraph.";
+}
